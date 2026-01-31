5ca8dd9164167fd785984c4cd33ae351cc64ce5d
Arnaud Gomes <arnaud.gomes.work@gmail.com>
2026-01-31 15:13:21 +0100

    x86 factorial

---
+++ software-security/x86/factorial.s
@@ -0,0 +1,13 @@
+.globl _start
+_start:
+    movl $1, %ecx   # Initialize counter n = 1
+    movl $1, %eax   # Initialize factorial result to 1
+factorial_loop:
+    cmpl $8, %ecx   # Compare counter with 8
+    jg end_factorial # If counter > 8, end loop
+    imull %ecx, %eax # Multiply result by counter
+    incl %ecx       # Increment counter
+    jmp factorial_loop # Repeat loop
+end_factorial:
+    ret
+    
